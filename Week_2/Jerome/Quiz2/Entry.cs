283bfce46e8d2a54be14871c20bf40a3f96b00ee
Jerome <jib92@gmx.net>
2017-10-22 20:37:51 +0200

    Lösung für die zweite Aufgabe

---
+++ Week_2/Jerome/Quiz2/Entry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2
+{
+    class Entry
+    {
+        public int Value { get; set; }
+        public Entry Left { get; set; }
+        public Entry Right { get; set; }
+    }
+}
