09e3ccb5cd633345ef19235f1c697e21c69301eb
Jan <neo17186@hotmail.com>
2018-01-12 21:37:23 +0100

    Year und Month hinzugefügt

--- Week_8/jan/database.js
+++ Week_8/jan/database.js
@@ -5,26 +5,33 @@ var transform = require('stream-transform');
 
 var db = new loki();
 const names = ["Date", "FIPS", "County", "Democrat - Active", "Republican - Active", "Libertarian - Active", "No Party - Active", "Other - Active", "Total - Active", "Democrat - Inactive", "Republican - Inactive", "Libertarian - Inactive", "No Party - Inactive", "Other - Inactive", "Total - Inactive", "Grand Total", "Primary Lat Dec", "Primary Long Dec", "Primary County Coordinates"];
+const template = {"Date": "", "Month": "", "Year": "", "FIPS": "",  "County": "",  "Democrat - Active": "",  "Republican - Active": "",  "Libertarian - Active": "",  "No Party - Active": "",  "Other - Active": "",  "Total - Active": "",  "Democrat - Inactive": "",  "Republican - Inactive": "",  "Libertarian - Inactive": "",  "No Party - Inactive": "",  "Other - Inactive": "",  "Total - Inactive": "",  "Grand Total": "",  "Primary Lat Dec": "",  "Primary Long Dec": "",  "Primary County Coordinates": ""};
 
+// Convert CSV line to object
 function toObject(data) {
-    var rv = {};
+    var rv = Object.assign({}, template);
     for (var i = 0; i < names.length; ++i)
         rv[names[i]] = data[i];
+    const date = new Date(data[0]);
+    if (date) {
+        rv.Month = date.getMonth().toString();
+        rv.Year = date.getFullYear().toString();
+    }
     return rv;
 }
 
+// Pipe file throught parser
 function parse_csv(filename) {
     var csv_collection = db.addCollection("csv");
     const parser = parse({delimiter: ','})
     const input = fs.createReadStream(filename);
     const transformer = transform(function(record, callback){
-    setTimeout(function(){
         csv_collection.insert(toObject(record));
-    }, 500);
-    }, {parallel: 10});
+    });
     input.pipe(parser).pipe(transformer).pipe(process.stdout);
 
     return csv_collection;
 }
 
+// Make it public
 module.exports = parse_csv;
--- Week_8/jan/server.js
+++ Week_8/jan/server.js
@@ -8,11 +8,13 @@ const filename = "data/State_of_Iowa_-_Monthly_Voter_Registration_Totals_by_Coun
 var parse_data = require('./database');
 var db = parse_data(filename);
 
+// Routing
 app.get('/api', function (req, res) {
   var response = db.find(req.query);
   res.send(response);
 });
 
+// Launch app
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
