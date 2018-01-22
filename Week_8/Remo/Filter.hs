e50dd1aa5c5cd0f7cc2d317eb06503fd1a743512
Remo Giermann <mo@liberejo.de>
2018-01-22 22:18:49 +0100

    Filter verallgemeinert und ein bisschen Refactoring

--- Week_8/Remo/Filter.hs
+++ Week_8/Remo/Filter.hs
@@ -1,83 +1,66 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Filter (filterAndLimit) where
+module Filter (filterRecords, limit, Accessor(..), FilterList) where
 
-import Iowa
-import CustomFields
-import Web.Scotty
-import Data.Time.Clock
+import Web.Scotty (Param)
+import Data.Time.Clock (UTCTime, utctDay)
 import Data.Time.Calendar (toGregorian)
 import Data.Text.Lazy.Read (decimal)
 import qualified Data.Text.Lazy as TL
 
-matchRecord ("fips", v) = compareText fips v
-matchRecord ("county", v) = compareText county v
-matchRecord ("grandTotal", v) = compareNumber grandTotal v
+-- Wrapper for accessor functions
+data Accessor a = AccessText     (a -> TL.Text) 
+                | AccessInt      (a -> Int)
+                | AccessMaybeInt (a -> (Maybe Int))
+                | AccessMonth    (a -> UTCTime)
+                | AccessYear     (a -> UTCTime)
 
-matchRecord ("democratActive", v) = compareMaybeNumber democratActive v
-matchRecord ("republicanActive", v) = compareMaybeNumber republicanActive v
-matchRecord ("libertarianActive", v) = compareMaybeNumber libertarianActive v
-matchRecord ("noPartyActive", v) = compareMaybeNumber noPartyActive v
-matchRecord ("otherActive", v) = compareMaybeNumber otherActive v
-matchRecord ("totalActive", v) = compareMaybeNumber totalActive v 
+type FilterList a = [(TL.Text, Accessor a)]
 
-matchRecord ("democratInactive", v) = compareMaybeNumber democratInactive v
-matchRecord ("republicanInactive", v) = compareMaybeNumber republicanInactive v
-matchRecord ("libertarianInactive", v) = compareMaybeNumber libertarianInactive v
-matchRecord ("noPartyInactive", v) = compareMaybeNumber noPartyInactive v
-matchRecord ("otherInactive", v) = compareMaybeNumber otherInactive v
-matchRecord ("totalInactive", v) = compareMaybeNumber totalInactive v 
+filterRecords :: FilterList a -> [Param] -> [a] -> [a]
+filterRecords flt pm rec = foldr filter rec . mkFilters flt $ pm 
 
-matchRecord ("month", v) = compareMonth date v
-matchRecord ("year", v) = compareYear date v
-matchRecord _ = const True
-
-filterAndLimit :: [Param] -> [VoterRegistration] -> [VoterRegistration]
-filterAndLimit pm = limit pm . filterRecords pm
-
-filterRecords :: [Param] -> [VoterRegistration] -> [VoterRegistration]
-filterRecords pm rec = foldr filter rec . paramToFilter $ pm 
-
-limit :: [Param] -> [VoterRegistration] -> [VoterRegistration]
+limit :: [Param] -> [a] -> [a]
 limit pm rec = case lookup "limit" pm of
                        Just (x) -> case decimal x of 
                                    Right (x', _) -> take x' rec
                                    Left _ -> rec
                        Nothing -> rec
 
-paramToFilter :: [Param] -> [VoterRegistration -> Bool]
-paramToFilter = map matchRecord
+mkFilters :: FilterList a -> [Param] -> [(a -> Bool)]
+mkFilters flt = map mkFilter
+    where mkFilter (k, v) = case lookup k flt of
+                             Nothing -> const True
+                             Just ac -> match ac v
 
+match :: Accessor a -> TL.Text -> (a -> Bool)
+match (AccessInt      get) arg = mkCompare arg . get                -- match a comparable Int (>100, <100, ..)
+match (AccessText     get) arg = TL.isPrefixOf (takeText arg) . get -- match a text (or prefix thereof)
+match (AccessMaybeInt get) arg = maybe False (mkCompare arg) . get  -- match a Maybe wrapped Int
+match (AccessMonth    get) arg = mkCompare arg . month . ymd . get  -- match the month in a date
+match (AccessYear     get) arg = mkCompare arg . year . ymd . get   -- match the year in a date
+
+-- Helper functions
+
+ymd dt = toGregorian $ utctDay dt
+year  (y,_,_) = fromInteger y
+month (_,m,_) = m
+day   (_,_,d) = d
 
 takeNum = decimal . prepComparable
 takeText = id
 
-getCompare v = case takeNum v of 
-                Right (x, cmp) -> case cmp of 
-                                  ">" -> (>x)
-                                  "<" -> (<x)
-                                  otherwise -> (==x)
-                Left _ -> const False
-
-compareNumber get arg = getCompare arg . get 
-
-compareText get arg = TL.isPrefixOf (takeText arg) . get
-
-compareMaybeNumber get arg = cmpHelp . get
-             where cmpHelp Nothing  = False
-                   cmpHelp (Just g) = getCompare arg g
-
-compareMonth get arg = cmp arg . get
-       where cmp s x = getCompare s $ getMonth x
-             getMonth dt = let (_, m, _) = toGregorian $ utctDay dt in m
-
-compareYear get arg = cmp arg . get
-       where cmp s x = getCompare s $ getYear x
-             getYear dt = let (y, _, _) = toGregorian $ utctDay dt in fromInteger y
+mkCompare :: TL.Text -> (Int -> Bool)
+mkCompare v = parse $ takeNum v 
+    where parse (Right (x, c)) = cmp x c
+          parse (Left   _    ) = const False
+          cmp x ">" = (>x)
+          cmp x "<" = (<x)
+          cmp x  _  = (==x)
 
 -- convert from ">9000" to "9000>"
 prepComparable t = case TL.take 1 t of 
-                 ">" -> (TL.drop 1 t) `mappend` ">"
-                 "<" -> (TL.drop 1 t) `mappend` "<"
-                 otherwise -> t
+                    ">" -> (TL.drop 1 t) `mappend` ">"
+                    "<" -> (TL.drop 1 t) `mappend` "<"
+                    otherwise -> t
 
