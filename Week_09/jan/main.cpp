ff14727d7fbd7b6ddadc14c53c3ec241a2a0d7e2
Jan <neo17186@hotmail.com>
2018-01-29 18:09:12 +0100

    Starting week 9 #Jan

---
+++ Week_09/jan/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+
+  for (int i = 0; i < 100; ++i)
+    std::cout << "Well done." << std::endl;
+
+
+    return 1;
+}
