af05a43314bdbcc6b56bff7767010f7562e82f1d
Jan <neo17186@hotmail.com>
2018-01-31 20:50:30 +0100

    ConsoleRunner als Test Unit hinzugefügt

--- Week_09/jan/main.cpp
+++ Week_09/jan/main.cpp
@@ -8,6 +8,26 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    if (argc > 1 && argv[1][0] == '-') {
+        switch (argv[1][1]) {
+            case 'd': {
+                try {
+                    auto const fn = Function::Factory::parse(argv[2]);
+                    std::cout << fn->print();
+                    std::cout << std::endl;
+                    std::cout << fn->derive();
+                } catch (...) {
+                    std::cout << "Parsing error" << std::endl;
+                }
+            }
+            break;
+            default:
+                std::cout << "Keine Angabe unter dieser Nummer. " << argv[1][1];
+            
+        }       
+        return 0;
+    } else {
+        testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    }
 }
